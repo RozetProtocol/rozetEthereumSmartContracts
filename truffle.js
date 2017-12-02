5c7d4968d09597e58faab2944045f4e6929a9eab
RozetPlatform <32420488+RozetPlatform@users.noreply.github.com>
2017-12-02 09:04:00 -0800

    Fix comment.

--- truffle.js
+++ truffle.js
@@ -5,6 +5,13 @@ module.exports = {
       port: 8545,
       network_id: "*", // Match any network id
       gas: 4600000
-    }
+    },
+    rinkby: {
+  	 host: "localhost", // Connect to geth on the specified
+      port: 8545,
+      from: "0xF69e96a3e03DAA29ceE9364cAd107219CB24C807", // default address to use for any transaction Truffle makes during migrations
+      network_id: 4,
+      gas: 4600000 // Gas limit used for deploys
+  	}
   }
 };
